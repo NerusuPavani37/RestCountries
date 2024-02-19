7c16db87ff1fd19bb3fef8e8876943b31b15b60e
NerusuPavani37 <nerusupavani3737@gmail.com>
2024-02-19 11:27:55 +0530

    darkMode using useContext

--- src/Components/ThemeContext.jsx
+++ src/Components/ThemeContext.jsx
@@ -7,17 +7,11 @@ export const useTheme = () => useContext(ThemeContext);
 export const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(()=>{
-    if(darkMode){
-      document.body.classList.add('dark-mode');
-    }else{
-      document.body.classList.remove('dark-mode');
-    }
-  },[darkMode]);
-
   const toggleDarkMode = () => {
     setDarkMode(prevDarkMode => !prevDarkMode);
   };
+
+  document.body.className=darkMode ? 'dark-mode' : '';
 
   return (
     <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
